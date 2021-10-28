5597f94771492b8e41a0c04ecf340b6490980734
yuri <yuriuniv@gmail.com>
2021-10-28 22:25:32 +0900

    Solve: 12918 (문자열 다루기 기본)

---
+++ Programmers/level 1/12918.py
@@ -0,0 +1,8 @@
+# 문자열 다루기 기본
+# https://programmers.co.kr/learn/courses/30/lessons/12918
+
+def solution(s):
+    if (len(s)==4 or len(s) == 6) and s.isdigit():
+        return True
+    else:
+        return False
